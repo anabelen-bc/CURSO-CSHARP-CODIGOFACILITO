a11d88f4ed56d3ed7e5dd05d076f8114a2396e36
anabelen-bc <anabelenn.bc@gmail.com>
2023-05-03 09:52:27 -0300

    Proyecto 6 - Modularizacion y Encapsulamiento

---
+++ Proyecto 6/Program.cs
@@ -0,0 +1,24 @@
+﻿using modularizacionYEncapsulamiento;
+
+namespace Presentacion6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            clsEmpleados1 empleado;
+            empleado = new clsEmpleados1();
+            empleado.Edad = 25;
+            empleado.Nombre = "Marcos Hernandez";
+            empleado.SueldoDiario = 12.5m;
+            decimal total;
+            total = empleado.CalculaSalario(30);
+            Console.WriteLine("El salario mensual del empleado "+ empleado.Nombre);
+            Console.WriteLine("Es: " + total.ToString("C"));
+            Console.ReadKey();
+
+
+        }
+    }
+} //La modularidad nos indica que tenemos que dividir nuestro programa en elementos identificables, y que se puedan utilizar para obtener nuestra solucion. Esta queda cubierta a traves de la creacion de las clases que representan nuestros objetos
+//Encapsulamiento(son campos), a traves de los metodos de propiedad, resguadamos las variables internas, y lo encapsulamos 
---
+++ Proyecto 6/clsEmpleados1.cs
@@ -0,0 +1,32 @@
+﻿namespace modularizacionYEncapsulamiento
+{
+    public class clsEmpleados1 
+    {
+        public clsEmpleados1()
+    {
+        Nombre = "";
+        SueldoDiario = 0.0m;
+        Edad = 0;
+    }
+
+    private string _Nombre;
+
+    public string Nombre
+    {
+        get { return Nombre1; }
+        set { Nombre1 = value; }
+    }
+
+        public string Nombre1 { get => _Nombre; set => _Nombre = value; 
+       }
+
+        public decimal SueldoDiario { get; set; }
+        public int Edad { get; set; }
+
+        public decimal CalculaSalario(int NumeroDias)
+        {
+            return SueldoDiario * NumeroDias;
+        }
+
+    }
+}
