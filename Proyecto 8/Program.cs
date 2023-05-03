461f14cd13772512d13b96ed4b4170054169366d
anabelen-bc <anabelenn.bc@gmail.com>
2023-05-03 09:55:57 -0300

    Proyecto 8 - ClasesAbstractas y Polimorfismo

---
+++ Proyecto 8/Program.cs
@@ -0,0 +1,17 @@
+﻿using clasesAbstractas;
+
+namespace Presentacion8
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            clsClientesConContacto cliente = new clsClientesConContacto(0, "Marcos Hernandez", "001", "HEHM", 1, "Marcos Hernandez", "254584", "45233687", "mh@hotmail.com", "Av tigre","145", "458", "La cueva", "Tuxtla", "Chiaps", "1619");
+        
+            Console.Write(cliente.Clave + " " + cliente.Nombre);
+            Console.WriteLine(cliente.TipoRegimen);
+            Console.WriteLine(cliente.RFC);
+            Console.ReadKey();
+        }
+    }
+}//Clases abstractas no se pueden instanciar - solo decimos las propiedades que va a tener esa y otras clases(estructura base)
